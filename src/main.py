4ec84c0d70cf57bfd55d5cf82ff09ab787f5fcdf
Kimberly <kimberly_93pc@hotmail.com>
2017-10-30 15:39:49 -0400

    Better commenting

--- src/main.py
+++ src/main.py
@@ -1,21 +1,22 @@
 import numpy as np;
 
-actorsFile = open("../res/additional_files/movie_actors.dat");
-directorsFile = open("../res/additional_files/movie_directors.dat");
-genresFile = open("../res/additional_files/movie_genres.dat");
-movieTagsFile = open("../res/additional_files/movie_tags.dat");
-tagsFile = open("../res/additional_files/tags.dat");
-testFile = open("../res/additional_files/test.dat");
-ttrainFile = open("../res/additional_files/train.dat");
-usersFile = open("../res/additional_files/user_taggedmovies.dat");
-
-
+actorsFile = open("../res/additional_files/movie_actors.dat", "r");
+directorsFile = open("../res/additional_files/movie_directors.dat", "r");
+genresFile = open("../res/additional_files/movie_genres.dat", "r");
+movieTagsFile = open("../res/additional_files/movie_tags.dat", "r");
+tagsFile = open("../res/additional_files/tags.dat", "r");
+testFile = open("../res/additional_files/test.dat", "r");
+ttrainFile = open("../res/additional_files/train.dat", "r");
+usersFile = open("../res/additional_files/user_taggedmovies.dat", "r");
 
 
 ''' 
-Creating the array of whatever the input is. 
-Only works for movie data. The index is the movie id
-Input is an initialized list, output is the array.
+* Creating an array for each movie data. 
+* Only works for movie data. 
+* The index where the element is in the 
+  array is the movie id, index=movieid.
+* Input is an initialized list, output 
+  is the array form of the data.
 '''
 def createMovieNumpy(file, list):
     for item in file:
@@ -26,14 +27,13 @@ def createMovieNumpy(file, list):
         list[int(tempList[0])-1].append(tempList[1:]);
     
     array = np.array(list);
-#     print(array);
     return array;
 
- #Actors
+#Actors
 actorsList = [[] for i in range(65133)];
 actorstArray = createMovieNumpy(actorsFile,actorsList);
  
- #Directors
+#Directors
 directorsList = [[] for i in range(65133)];
 directorsArray = createMovieNumpy(directorsFile,directorsList);
  
@@ -41,6 +41,6 @@ directorsArray = createMovieNumpy(directorsFile,directorsList);
 genresList = [[] for i in range(65133)];
 genresArray = createMovieNumpy(genresFile,genresList);
 
-#Tags
+#Movie Tags
 movieTagsList = [[] for i in range(65133)];
 movieTagsArray = createMovieNumpy(movieTagsFile,movieTagsList);
